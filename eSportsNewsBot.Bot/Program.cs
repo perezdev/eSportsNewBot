94fb0b72038cf6b868201b5550caef810aaad0a9
Ernest D Perez Jr <ernest.perezjr@gmail.com>
2018-02-03 20:31:19 -0600

    Added starter code for the bot execution

--- eSportsNewsBot.Bot/Program.cs
+++ eSportsNewsBot.Bot/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eSportsNewsBot.Bot.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,36 @@ namespace eSportsNewsBot.Bot
 {
     class Program
     {
-        static void Main(string[] args)
+        public static WebsiteService _websiteService { get; set; } = new WebsiteService();
+
+        private static void Main(string[] args)
         {
+            ExecuteCollection();
+            ExecutePost();
+        }
+
+        private static void ExecuteCollection()
+        {
+            try
+            {
+                var websites = _websiteService.GetActiveWebsites();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private static void ExecutePost()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
